cc6f3add2c16cbadd227be9b1b58df1363d2eb86
Slaazy <44474175+Slaazy@users.noreply.github.com>
2018-11-18 19:08:33 -0200

    Add files via upload

---
+++ Lista de Exercícios 2 - Questão9.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Questão9 {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		
+		float A,B,C;
+		
+		System.out.println("Informe o primeiro lado");
+		A = x.nextFloat();
+		System.out.println("informe o segundo lado");
+		B = x.nextFloat();
+		System.out.println("informe o terceiro lado");
+		C = x.nextFloat();
+		
+		if (A + B > C || A + C > B || B + C > A) {
+			if (A == B && B == C) {
+				System.out.println("Equilatero");
+			} 
+		   else 
+			   if(A == B && C != B || B==C && A!=B || A==C && B!=A) {
+				System.out.println("Isósceles");
+			}
+		   else  
+		       if(A!=B && A!=C && B!=C) {
+			    System.out.println("Escaleno");
+		       }
+			    
+		        else 
+		    	   System.out.println("não e um triângulo");
+		}
+	}
+}
+		       
+	
+
+			    
+			    
+	
+			
+		
+	
+
+		       	
+
+
+
+
