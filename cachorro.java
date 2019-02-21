42c24c16dd3d24b2cdea12147b8ede87a76978eb
Slaazy <44474175+Slaazy@users.noreply.github.com>
2019-02-20 21:50:23 -0300

    Add files via upload

---
+++ cachorro.java
@@ -0,0 +1,32 @@
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+
+
+
+
+/**
+ *
+ * @author FMN
+ */
+public class cachorro {
+    private String nome;
+    private String raça;
+    private String cor;
+    private int cartão;
+    private double valor;
+    private double sexo;
+    private date nascimento;
+    private double tamanho;
+    
+    public cachorro(String c,String se){
+        cor = c;
+        raça = se;
+                 
+    }
+    public void correr() {
+        
+    }
+
+   
+    
+
