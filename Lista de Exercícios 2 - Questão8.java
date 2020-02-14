b842b3b233175c8b0424dd718a05cae769a4a06c
Slaazy <44474175+Slaazy@users.noreply.github.com>
2020-02-14 15:06:55 -0300

    Update Lista de Exercícios 2 - Questão8.java

--- Lista de Exercícios 2 - Questão8.java
+++ Lista de Exercícios 2 - Questão8.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class Quest„o8 {
+public class Quest√£o8 {
 
+//salario com reajuste,percentual do aumento,valor do aumento,salario tera aumento de:
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
 		
@@ -12,30 +13,30 @@ public class Quest„o8 {
 		salario = x.nextFloat();
 		
 		if (salario<280) {
-			System.out.println("seu sal·rio antes do reajuste " + salario);
-			System.out.println("seu percentual de aumento ser· de 20%");
-			System.out.println("o valor do aumento ser· "+ (salario-(salario*0.20)));
-			System.out.println("seu sal·rio ter· aumento de "+ salario*0.2);
+			System.out.println("seu sal√°rio antes do reajuste " + salario);
+			System.out.println("seu percentual de aumento ser√° de 20%");
+			System.out.println("o valor do aumento ser√° "+ (salario-(salario*0.20)));
+			System.out.println("seu sal√°rio ter√° aumento de "+ salario*0.2);
 			
 		} else if (salario >= 280 && salario<700 ) {
-			System.out.println("seu sal·rio antes do reajuste ser· de " + salario);
-			System.out.println("seu percentual de aumento ser· de 15%");
-			System.out.println("o valor do aumento ser· " +(salario+(salario*0.15)));
-			System.out.println("seu sal·rio final com reajuste ser· de "+ salario*0.15);
+			System.out.println("seu sal√°rio antes do reajuste ser√° de " + salario);
+			System.out.println("seu percentual de aumento ser√° de 15%");
+			System.out.println("o valor do aumento ser√° " +(salario+(salario*0.15)));
+			System.out.println("seu sal√°rio final com reajuste ser√° de "+ salario*0.15);
 			
 
 		} else if (salario>=700 && salario<1500) {
-			System.out.println("seu sal·rio antes do reajuste ser· de " + salario);
-			System.out.println("seu percentual de aumento ser· de 10%");			
-			System.out.println("o valor do aumento ser· "+ (salario-(salario*0.10)));
-			System.out.println("seu sal·rio final com reajuste ser· de "+ salario*0.10);
+			System.out.println("seu sal√°rio antes do reajuste ser√° de " + salario);
+			System.out.println("seu percentual de aumento ser√° de 10%");			
+			System.out.println("o valor do aumento ser√° "+ (salario-(salario*0.10)));
+			System.out.println("seu sal√°rio final com reajuste ser√° de "+ salario*0.10);
 			
 
 		}else if (salario>=1500) {
-			System.out.println("seu sal·rio final com reajuste ser· de "+ salario*0.10);
-			System.out.println("seu percentual de aumento ser· de 5%");
-			System.out.println("o valor do aumento ser· "+ (salario-(salario*0.05)));
-			System.out.println("seu sal·rio ser· de "+ salario*0.05);
+			System.out.println("seu sal√°rio final com reajuste ser√° de "+ salario*0.10);
+			System.out.println("seu percentual de aumento ser√° de 5%");
+			System.out.println("o valor do aumento ser√° "+ (salario-(salario*0.05)));
+			System.out.println("seu sal√°rio ser√° de "+ salario*0.05);
 		
 			
 		}else if (salario == 0) {
